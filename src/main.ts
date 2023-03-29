64a0f8f8642993fdbc510503755b8cd2daf7a15d
Benjamin Chang <bkchang@students.cs.ubc.ca>
2023-03-28 19:34:38 -0700

    implemented initial histogram

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,24 @@ import { Histogram } from './histogram';
 import { BarChart } from './barChart';
 
 const dispatch = d3.dispatch('lmaoidk');
-// console.log("HELLO WORLD!");
-// TODO: load csv and map with `toSalaryRecord`
 let data: SalaryRecord[] = [];
 
 d3.csv('data/salaries_data.csv').then(_data => {
-    // console.log('hello');
-    console.log(_data)
+    // console.log(_data)
     _data.forEach(d => {
-        // console.log(d)
-        // console.log(toSalaryRecord(d))
         data.push(toSalaryRecord(d))
+        // console.log(data)
       });
-    // console.log("NEW DATA");
-    // console.log(data);
+
+      const histogram = new Histogram(data, {
+        parentElement: '#histogram',
+        width: 500,
+        height: 500,
+        margin: { top: 10, right: 10, bottom: 50, left: 50 }
+    });
+    histogram.updateVis();
 })
+
 
 const scatterPlot = new ScatterPlot(data, {
     parentElement: '#scatter-plot',
@@ -27,12 +30,7 @@ const scatterPlot = new ScatterPlot(data, {
     height: 500,
     margin: { top: 10, right: 10, bottom: 50, left: 50 }
 });
-const histogram = new Histogram(data, {
-    parentElement: '#histogram',
-    width: 500,
-    height: 500,
-    margin: { top: 10, right: 10, bottom: 50, left: 50 }
-});
+
 const barChart = new BarChart(data, {
     parentElement: '#bar-chart',
     width: 500,
@@ -40,5 +38,4 @@ const barChart = new BarChart(data, {
     margin: { top: 10, right: 10, bottom: 50, left: 50 }
 });
 scatterPlot.updateVis();
-histogram.updateVis();
 barChart.updateVis();
